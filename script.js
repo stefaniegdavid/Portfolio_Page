b2cf9eb030fc23f2e1203f87bb94d767b676632f
Stefanie David <127105234+stefaniegdavid@users.noreply.github.com>
2023-04-28 00:17:42 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+var cursor = document.getElementById("cursor");
+document.onmousemove = function(e){
+    cursor.style.left = (e.pageX - 25) + "px";
+    cursor.style.top = (e.pageY - 25) + "px";
+    cursor.style.display = "block";
+}
 
-var header = document.querySelector('header');
 
-menubar.onClick
+// let cursor = document.querySelector('#cursor');
+
+// window.onmousemove = (e) =>{
+//     cursor.style.top = e.pageY + 'px';
+//     cursor.style.left = e.pageX + 'px';
+// }
+
+document.querySelectorAll('a').forEach(links =>{
+links.onmouseenter = () =>{
+    cursor.classList.add('active');
+}
+links.onmouseleave = () =>{
+    cursor.classList.remove('active');
+}
+
+
+});
